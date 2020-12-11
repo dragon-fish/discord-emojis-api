b73cdcfa05a516e08deb5aec42b5bfbdd6ee6b3d
Dragon-Fish <824399619@qq.com>
2020-12-12 00:08:21 +0800

    Minor fix

---
+++ api/emojis/[id].js
@@ -0,0 +1,20 @@
+const getImg = require('node-base64-image').encode
+
+module.exports = async (req, res) => {
+  const { id } = req.query
+  if (!id || !/^[0-9]+$/.test(id)) {
+    res.status(404).send({
+      error: 'Invalid id'
+    })
+    return
+  }
+  getImg('https://cdn.discordapp.com/emojis/' + id + '.png').then(
+    (img) => {
+      res.setHeader('Content-Type', 'image/png')
+      res.status(200).send(img)
+    },
+    (err) => {
+      res.status(503).send(err)
+    }
+  )
+}
